6e47ee933a3ddbbd25c67b5fd15f5a899ae50f67
Jeff Foxx <foxx@adobe.com>
2025-09-09 16:51:35 -0400

    event cards

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,15 +1,57 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
+function events(parent) {
+  const paragraphs = parent.querySelectorAll('p');
+  paragraphs.forEach((p) => {
+    const text = p.textContent.trim();
+
+    if (text.startsWith('Location:')) {
+      p.textContent = text.replace(/^Location:\s*/, '');
+      p.classList.add('location');
+    }
+
+    if (text.startsWith('Date:')) {
+      p.textContent = text.replace(/^Date:\s*/, '');
+      p.classList.add('date');
+    }
+  });
+  const details = document.createElement('div');
+  details.className = 'details';
+
+  const location = parent.querySelector('.location');
+  const date = parent.querySelector('.date');
+  if (date) {
+    details.appendChild(date);
+  }
+  if (location) {
+    details.appendChild(location);
+  }
+  if (details.children.length > 0) {
+    parent.appendChild(details);
+  }
+}
+
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
+    const a = document.createElement('a');
+    const link = block.querySelector('a');
+    if (link) {
+      a.href = link.href;
+      a.target = link.target;
+      a.rel = link.rel;
+    }
+    li.append(a);
+    while (row.firstElementChild) a.append(row.firstElementChild);
+    [...a.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
+    if (block.classList.contains('events')) {
+      events(a);
+    }
     ul.append(li);
   });
 
